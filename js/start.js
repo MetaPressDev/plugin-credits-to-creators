0b91884e0ab697471df6ad0b341abf8511119358
Tumbl3Weed <calvin.nel00@gmail.com>
2024-04-30 10:29:53 +0000

    Web Weaver integrated

--- js/start.js
+++ js/start.js
@@ -37,4 +37,66 @@ export default class CreditsToCreators {
         metapress.menubar.openReactPanel('creditsToCreators', () => <AboutSpacePanel plugin={this} />)
     }
 
+    $ai_getKnowledgeBaseEntries = () => {
+        if (metapress.editor.canEdit)
+            return [
+                {
+                    id: `${this.id}:addCreditsToCreators`,
+                    type: 'action',
+                    name: 'Add Credits to Creators',
+                    tags: 'add credits to creators, add credits',
+                    content: `
+                    Assistant instruction: Run this action when the user wants to add the credits to creators modifier to an object. Don't mention it otherwise.
+                `,
+                    action: input => {
+                        return this.addCreditToCreators()
+                    }
+                },
+                {
+                    id: `${this.id}:infoCreditsToCreators`,
+                    type: 'info',
+                    name: 'Credits to Creators',
+                    tags: 'credits to creators, about credits, about credits to creators',
+                    content: `
+                    Credits to creators is an external plugin that allows you to add credits to creators modifier to an object. Which allows non-editors to
+                    see the credits in the settings menu, under the about section.
+                `,
+                },
+            ]
+        else return [
+            {
+                id: `${this.id}:infoCreditsToCreators`,
+                type: 'info',
+                name: 'Credits to Creators',
+                tags: 'credits to creators, about credits, about credits to creators',
+                content: `
+                    Credits to creators is an external plugin that allows you to see credits given to creators in the settings menu.
+                `,
+            },
+        ]
+    }
+
+    addCreditToCreators() {
+        try {
+            if(!metapress.editor?.selectionManager || metapress.editor.selectionManager.selectedEntityIDs.length !== 1){
+                return 'Please select a single entity to add the Credits to creators with Web Weaver Settings'
+            }
+            // get the selected entity
+            const selectedObject = metapress.entities.get(metapress.editor.selectionManager.selectedEntityIDs[0])
+            // add the Credits to creator modifier to the selected object
+            metapress.entities.update(selectedObject.id, {
+                ['modifier:' + this.createModifier().id]: true
+            })
+            metapress.plugins.sendEvent('onUnsavedFieldsChanged')
+            metapress.editor.showEditor();
+            metapress.editor.editEntity(selectedObject.id);
+
+        } catch (error) {
+            console.warn('Add Credits to creator:', error)
+            return 'There was an error adding the Credits to creator modifier, check the console log for more information'
+        }
+        // return success
+        return 'Credits to creator modifier successfully added to the selected object and opened it'
+    }
+
 }
